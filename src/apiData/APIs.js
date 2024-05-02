9bb8182af99999fdcca339e152d794485fbc43ca
CheapSharkTank <swachchha4223@gmail.com>
2024-05-02 13:49:26 +0600

    responsive

--- src/apiData/APIs.js
+++ src/apiData/APIs.js
@@ -1,2 +1,2 @@
-export const openWeatherApi = "645fa63d1e70bbf8cb7410c055c1fb04";
-export const weatherApi = "ae5e5a91670d4325aaf132012242204";
+export const openWeatherApi = "YOUR API KEY HERE";
+export const weatherApi = "YOUR API KEY HERE";
